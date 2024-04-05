23acfb90076e7a3be547a07b745387a2cc4328ae
Alex Yang <alexyoung0204@gmail.com>
2024-04-05 21:52:20 +0000

    dataproject1 step1-4

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app.mount("/static", StaticFiles(directory="static", html = True), name="static"
 DBHOST = os.environ.get('DBHOST')
 DBUSER = os.environ.get('DBUSER')
 DBPASS = os.environ.get('DBPASS')
-DB = "nem2p"
+DB = "zy7ts"
 
 @app.get("/")  # zone apex
 def zone_apex():
@@ -26,4 +26,12 @@ def add_me(number_1: int, number_2: int):
     sum = number_1 + number_2
     return {"sum": sum}
 
+@app.get("/albums/{id}")
+def get_albums(id):
+    db = MySQLdb.connect(host=DBHOST, user=DBUSER, passwd=DBPASS, db=DB)
+    c = db.cursor(MySQLdb.cursors.DictCursor)
+    c.execute("SELECT * FROM albums WHERE id=" + id)
+    results = c.fetchall()
+    db.close()
+    return results
 # More will go here TBD
